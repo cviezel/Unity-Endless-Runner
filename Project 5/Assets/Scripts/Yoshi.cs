e6e10a877e66b5c28fd5e8cd79db82d47a91fc5d
cviezel <cviezel@gmail.com>
2019-04-01 17:31:15 -0400

    fixed jumping and phase animations

--- Project 5/Assets/Scripts/Yoshi.cs
+++ Project 5/Assets/Scripts/Yoshi.cs
@@ -1,51 +1,74 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Yoshi : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public Animator anim;
-    public AudioSource jump;
-    public AudioSource double_jump;
-    public AudioSource death;
-    void Start()
-    {
-      anim = GetComponent<Animator>();
-    }
+  // Start is called before the first frame update
+  public Animator anim;
+  public Rigidbody2D rb;
+  public AudioSource jump;
+  public AudioSource double_jump;
+  public AudioSource death;
+  public BoxCollider2D col;
+  int jumps;
+  void Start()
+  {
+    anim = GetComponent<Animator>();
+    rb = GetComponent<Rigidbody2D>();
+    col = GetComponent<BoxCollider2D>();
+    jumps = 2;
+  }
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    Vector2 position = this.transform.position;
+    Quaternion rotation = this.transform.rotation;
+    position.x = -2;
+    rotation.z = 0;
+    this.transform.position = position;
+    this.transform.rotation = rotation;
+
+    if(rb.velocity.y > 0) //phases through platforms if going up
     {
-      Vector2 position = this.transform.position;
-      Quaternion rotation = this.transform.rotation;
-      position.x = -2;
-      rotation.z = 0;
-      this.transform.position = position;
-      this.transform.rotation = rotation;
-        if (Input.GetKeyDown("space"))
-        {
-          if(anim.GetBool("Jump") == false)
-          {
-            jump.Play();
-            anim.SetBool("Jump", true);
-            anim.SetBool("DoubleJump", false);
-          }
-          else
-          {
-            double_jump.Play();
-            anim.SetBool("DoubleJump", true);
-          }
-          position.y += 5;
-          this.transform.position = position;
-        }
+      col.isTrigger = true;
     }
-    void OnCollisionEnter2D (Collision2D col)
+    else
     {
-      if(col.gameObject.tag.Equals("Platform") && anim.GetBool("Jump") == true)
+      col.isTrigger = false;
+    }
+    if (Input.GetKeyDown("space") && jumps > 0)
+    {
+      if(anim.GetBool("Jump") == false) //single jump
       {
-        anim.SetBool("Jump", false);
-        anim.SetTrigger("Land");
+        jump.Play();
+        anim.SetBool("Jump", true);
+        anim.SetBool("DoubleJump", false);
+        rb.velocity = new Vector2(0,0);
+        rb.AddForce(Vector2.up * 350);
+        jumps--;
       }
+      else //double jump
+      {
+        double_jump.Play();
+        anim.SetBool("DoubleJump", true);
+        rb.velocity = new Vector2(0,0);
+        rb.AddForce(Vector2.up * 300);
+        jumps--;
+      }
+
+      //position.y += 5;
+      //this.transform.position = position;
     }
+  }
+  void OnCollisionEnter2D (Collision2D col)
+  {
+    if(col.gameObject.tag.Equals("Platform") && anim.GetBool("Jump") == true)
+    {
+      anim.SetBool("Jump", false);
+      anim.SetTrigger("Land");
+      jumps = 2;
+    }
+  }
 }
